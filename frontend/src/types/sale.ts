4e639259161050cc69aacab5160bf13cde6d8a54
cspreng <carlos.spreng@gmail.com>
2021-11-05 17:01:48 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,11 @@
 export type SaleSum = {
-    sellerName: string,
+    sellerName: string;
     sum: number
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+
 }
